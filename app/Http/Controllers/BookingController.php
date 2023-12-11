0cc1b26524509d85baba836bd69d4b8730827287
lokesh kumar jha <lokesh@synergicsoftek.com>
2023-12-11 17:46:33 +0530

    chnage

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -825,7 +825,7 @@ class BookingController extends Controller
     public function hdfcorderStatusTracker(Request $request){
 
         $merchant_json_data =array('order_no' => $request->booking_id,'reference_no' =>$request->reference_no);
-        print_r($merchant_json_data);
+        //print_r($merchant_json_data);
         $working_key='82C2335B9118D35E9BB7A7112E32215D';//Shared by CCAVENUES
         $access_code='AVND18KJ61AM21DNMA';//Shared by CCAVENUES
         $merchant_data = json_encode($merchant_json_data);
