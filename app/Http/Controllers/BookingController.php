1a1183e55ce6ddba38013015d3c32894e8ee5b6b
lokesh kumar jha <lokesh@synergicsoftek.com>
2023-12-12 10:50:27 +0530

    chnage

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -835,7 +835,7 @@ class BookingController extends Controller
         curl_setopt($ch, CURLOPT_URL, "https://apitest.ccavenue.com/apis/servlet/DoWebTrans");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_VERBOSE, 1);
-       // curl_setopt($ch, CURLOPT_HTTPHEADER,'Content-Type: application/json') ;
+        curl_setopt($ch, CURLOPT_HTTPHEADER,'Content-Type: application/json') ;
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $final_data);
